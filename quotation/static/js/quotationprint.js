827695348c25d2b407535830f77b051c49cec94e
Mate Szluka <szluka.mate@gmail.com>
2019-01-26 21:46:50 +0100

    Quotation maker 2.5.3 printing demo Vikinek

--- quotation/static/js/quotationprint.js
+++ quotation/static/js/quotationprint.js
@@ -13,46 +13,76 @@ $(function () {
 
 var height;
 var sumheight=0;
-var itemnumbers=$('#itemnumberp').attr( "itemnumber" );
-itemnumbers--;
+var itemnumbers;
+itemnumbers();
 var towrap = [];
 var i;
 var wrapphrase="";
-for (i = 0; i <= itemnumbers; i++) {
+var minitemwrapped;
+var maxitemwrapped;
+var pagenumber=1;
+main();
 
-    height=$('div[class="item-container"][rowid="' + i + '"').outerHeight();
-    sumheight=sumheight+height;
 
-console.log(i + " " + height + " " + sumheight);
 
-    if (sumheight > 600 || i == itemnumbers) {
-                   if (i == itemnumbers) {towrap.push(i);} //last record to wrap we put to array
-      //$('span[name="htmlinsert"][rowid="' + i + '"').html('<p>TEXTT</p>');
-            for (j = 0; j < towrap.length; j++) {
 
-            wrapphrase= wrapphrase + "div[class=\"item-container\"][rowid=\"" + towrap[j] + "\"], "
+    function main(){
+        for (i = 0; i <= itemnumbers; i++) {
+
+            measureitemcontainer();
+
+            if (sumheight > 600 ) { // sumheight: sum height of items inspected via cycle
+                wrap();
+            }
+            if (i == itemnumbers) { //last page
+            towrap.push(i); //last record to wrap we put to array and wrap
+            wrap();
+            pagenumberer();
+            return;
+            }
+            towrap.push(i); //if there is room on page only goes to wrap array
+
+        }
+
+    }
+
+    function wrap(){
+        pagenumber++;
+        minitemwrapped=towrap[0];
+        maxitemwrapped=towrap[towrap.length-1];
+          for (j = 0; j < towrap.length; j++) {
+
+            wrapphrase= wrapphrase + "span[name=\"htmlinsertbefore\"][rowid=\"" + towrap[j] + "\"], div[class=\"item-container\"][rowid=\"" + towrap[j] + "\"], span[name=\"htmlinsertafter\"][rowid=\"" + towrap[j] + "\"], "
             }
         wrapphrase = wrapphrase.slice(0, -2); // remove comma from end (-2 cause there is an adding space)
         jQuery.globalEval( "$('" + wrapphrase + "').wrapAll('<div class=\"page\"> </div>');" );
         sumheight=0;
         towrap.length=0;
         wrapphrase="";
+        headerinsert(minitemwrapped); // which item insertbefore
+        footerinsert(maxitemwrapped, pagenumber);
+
     }
-    towrap.push(i);
+    function headerinsert(rowid){
+    $('span[name="htmlinsertbefore"][rowid="' + rowid + '"').html('<div class="header" ><div class="headerlabels" ><span style="margin-left: 40mm" >Description</span><span style="margin-left: 60mm" >Qty</span></div></div>');
+    }
+    function footerinsert(rowid, pagenumber){
+    $('span[name="htmlinsertafter"][rowid="' + rowid + '"').html('<div class="footerdiv" ><hr><span class="footerspan1">Please see the conditions in details on the last page<br></span><span pagenumber=\"' + pagenumber + '\" class="footerspan1">Description2</span></div>');
+    }
+    function measureitemcontainer(){
+    height=$('div[class="item-container"][rowid="' + i + '"').outerHeight();
+    sumheight=sumheight+height;
+    }
+    function itemnumbers(){
+    itemnumbers=$('#itemnumberp').attr( "itemnumber" ); //number of products
+    itemnumbers--;
+    }
+    function pagenumberer(){
+            for (k = 1; k != pagenumber+1; k++) {
+            $('span[class="footerspan1"][pagenumber="' + k + '"').text( "page " + k + "/" + pagenumber);
 
 
-    //if sumheight > 1400 then
-    //    insert footer/break/header/page closing div/page opening div before this item
-    // the page number = number of insert
+            }
 
-
-
- }
-
-
-
- //$('div[class="item-container"][rowid="' + 0 + '"], div[class="item-container"][rowid="' + 1 + '"]' ).wrapAll('<div class="page"> </div>');
-              //div[class="item-container"][rowid="7"], div[class="item-container"][rowid="8"], div[class="item-container"][rowid="9"] //from console
- //    put page number to footer
-
+    }
 });
