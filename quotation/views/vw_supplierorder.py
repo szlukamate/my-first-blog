0d72ae57049be451634f427db1857a3d113b9308
Mate Szluka <szluka.mate@gmail.com>
2019-04-28 22:41:23 +0200

    Quotation maker supplierorderpre

--- quotation/views/vw_supplierorder.py
+++ quotation/views/vw_supplierorder.py
@@ -11,6 +11,61 @@ from datetime import datetime, timedelta
 # pdb.set_trace()
 
 def supplierorderpre(request):
-    return render(request, 'quotation/supplierorderpre.html', {})
+    cursor3 = connection.cursor()
+    cursor3.execute("SELECT  "
+                    "Docid_tblDoc, "
+                    "Contactid_tblDoc_id, "
+                    "Doc_kindid_tblDoc_id, "
+                    "companyname_tblcompanies_ctbldoc, "
+                    "firstname_tblcontacts_ctbldoc, "
+                    "lastname_tblcontacts_ctbldoc, "
+                    "prefacetextforquotation_tblprefaceforquotation_ctbldoc, "
+                    "backpagetextforquotation_tblbackpageforquotation_ctbldoc, "
+                    "prefacespecforquotation_tbldoc, "
+                    "subject_tbldoc, "
+                    "docnumber_tbldoc, "
+                    "creatorid_tbldoc, "
+                    "creationtime_tbldoc, "
+                    "title_tblcontacts_ctbldoc, "
+                    "mobile_tblcontacts_ctbldoc, "
+                    "email_tblcontacts_ctbldoc, "
+                    "pcd_tblcompanies_ctbldoc, "
+                    "town_tblcompanies_ctbldoc, "
+                    "address_tblcompanies_ctbldoc, "
+                    "total_tbldoc, "
+                    "deliverydays_tbldoc, "
+                    "paymenttextforquotation_tblpayment_ctbldoc, "
+                    "currencycodeinreport_tbldoc, "
+                    "currencyrateinreport_tbldoc, "
+                    "accountcurrencycode_tbldoc, "
+                    "pretag_tbldockind, "
+
+                    "`Doc_detailsid_tblDoc_details`, "
+                    "`Qty_tblDoc_details`, "
+                    "`Docid_tblDoc_details_id`, "
+                    "`customerdescription_tblProduct_ctblDoc_details`, "
+                    "`firstnum_tblDoc_details`, "
+                    "`fourthnum_tblDoc_details`, "
+                    "`secondnum_tblDoc_details`, "
+                    "`thirdnum_tblDoc_details`, "
+                    "`Note_tblDoc_details`, "
+                    "`creationtime_tblDoc_details`, "
+                    "currencyisocode_tblcurrency_ctblproduct_ctblDoc_details, "
+                    "Productid_tblDoc_details_id, "
+                    "Doc_detailsid_tblDoc_details, "
+                    "unit_tbldocdetails, "
+                    "S.companyname_tblcompanies as supplier, "
+
+                    "supplierdescription_tblProduct_ctblDoc_details "
+                    "FROM quotation_tbldoc_details as DD "
+                    "JOIN quotation_tblcompanies as S ON DD.suppliercompanyid_tbldocdetails = S.companyid_tblcompanies "
+                    "JOIN quotation_tbldoc as D ON D.Docid_tblDoc=DD.Docid_tblDoc_details_id "
+                    "JOIN quotation_tbldoc_kind as DK ON D.Doc_kindid_tblDoc_id = DK.Doc_kindid_tblDoc_kind "
+                    "WHERE Doc_kindid_tblDoc_id=2 and obsolete_tbldoc=0 "
+                    "order by firstnum_tblDoc_details,secondnum_tblDoc_details,thirdnum_tblDoc_details,fourthnum_tblDoc_details")
+    customerorders = cursor3.fetchall()
+
+
+    return render(request, 'quotation/supplierorderpre.html', {'customerorders': customerorders})
 
 
