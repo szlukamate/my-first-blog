e14c750e183b663b84fe12a31232162a9bd2d5aa
Mate Szluka <szluka.mate@gmail.com>
2020-04-03 23:01:43 +0200

    Aid stepper2

--- aid/static/js/aorderprocess.js
+++ aid/static/js/aorderprocess.js
@@ -10,26 +10,71 @@ localStorage.lastname = "Smith";
 // Retrieve
  console.log(localStorage.lastname);
 $(function () {
+    var currentstep = 0;
+    var maxstep = 3;
+    var othertexttotab = 2;
+
     $( "#tabs" ).tabs({
         active: 0,  //run when initialized
-        activate: function(event, ui) {  //run when clicked
-            //  Get future value
-            var newIndex1 = ui.newTab.parent().children().index(ui.newTab);
-             console.log(newIndex1);
-
-            //  Set future value
-            //try {
-            //    dataStore1.setItem( index1, newIndex1 );
-            //} catch(e) {}
-        }
+        //disabled:  disabledstep
     });
+    $( "#tabs" ).tabs({ disabled: true });
+    $( "#tabs" ).tabs("enable", 0);
+//    $('#tabs ul:first li:eq(' & othertexttotab & ') a').text("Other text");
+    $('#tabs ul:first li:eq(' + othertexttotab + ') a').text("Other text");
 
-//    $( "#tabs" ).tabs( { disabled: [0, 1] } );
+    $('#previousbutton').prop("disabled", true)
+    $('#finishbutton').prop("disabled", true)
+
     $('#nextbutton').click(function() {
-             console.log('newIndex1');
-        $( "#tabs" ).tabs({
-            active: 1,  //run when initialized
-        });
+        console.log('nextbuttonclick');
+        currentstep++;
+        tabactivator();
+    });
+    $('#previousbutton').click(function() {
+        console.log('previousbuttonclick');
+        currentstep--;
+        tabactivator();
 
     });
+    $('#finishbutton').click(function() {
+        console.log('finishbuttonclick');
+        location.reload();
+    });
+    function tabactivator(){
+            if (currentstep == 0 ) {
+                $( "#tabs" ).tabs( "option", "disabled", [ 1, 2, 3 ] )
+
+                $( "#tabs" ).tabs({
+                    active: 0,
+                });
+                $('#previousbutton').prop("disabled", true)
+
+            }
+
+            if ((currentstep !== 0) && (currentstep !== maxstep )) {
+                $( "#tabs" ).tabs( "option", "disabled", [ 0, 2 ] )
+
+                $( "#tabs" ).tabs({
+                    active: 1,
+                });
+                $('#previousbutton').prop("disabled", false)
+                $('#finishbutton').prop("disabled", true)
+                $('#nextbutton').prop("disabled", false)
+
+            }
+
+            if (currentstep == maxstep ) {
+                $( "#tabs" ).tabs( "option", "disabled", [ 0, 1 ] )
+
+                $( "#tabs" ).tabs({
+                    active: 2,
+                });
+                $('#nextbutton').prop("disabled", true)
+                $('#finishbutton').prop("disabled", false)
+
+            }
+
+    }
+
 });
