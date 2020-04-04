54379f333d182fdbbba24ae46a6312c36bd7c41e
Mate Szluka <szluka.mate@gmail.com>
2020-04-04 22:45:09 +0200

    Aid stepper3

--- aid/static/js/aorderprocess.js
+++ aid/static/js/aorderprocess.js
@@ -1,80 +1,78 @@
 /*
 aorderprocess.js
 */
-
             var msg="Hello Javascript2";
                     console.log(msg);
-
                     // Store
 localStorage.lastname = "Smith";
 // Retrieve
  console.log(localStorage.lastname);
 $(function () {
     var currentstep = 0;
-    var maxstep = 3;
-    var othertexttotab = 2;
-
-    $( "#tabs" ).tabs({
-        active: 0,  //run when initialized
-        //disabled:  disabledstep
+    var maxstep = 3; //only this parameter is needed to adjust if the number of tabs would change
+    //initialize tabs
+    $( "#tabs" ).tabs({ //first tab let be active
+        active: 0,
     });
-    $( "#tabs" ).tabs({ disabled: true });
-    $( "#tabs" ).tabs("enable", 0);
-//    $('#tabs ul:first li:eq(' & othertexttotab & ') a').text("Other text");
-    $('#tabs ul:first li:eq(' + othertexttotab + ') a').text("Other text");
+    $( "#tabs" ).tabs({ disabled: true }); //all tabs are disable
+    $( "#tabs" ).tabs("enable", 0); //but first
+    $('#previousbutton').prop("disabled", true) //default previousbutton is disabled
+    $('#finishbutton').prop("disabled", true) //default finishbutton is disabled
 
-    $('#previousbutton').prop("disabled", true)
-    $('#finishbutton').prop("disabled", true)
-
+    //nextbutton eventlistener script
     $('#nextbutton').click(function() {
-        console.log('nextbuttonclick');
+        setactivetabtextfilled('next'); //add ' - OK' to the end of tab
         currentstep++;
-        tabactivator();
+        tabactivator(); //set appropriate tab to active
     });
+    //previousbutton eventlistener script
     $('#previousbutton').click(function() {
-        console.log('previousbuttonclick');
+        setactivetabtextfilled('previous'); //remove ' - OK' from previous tab
         currentstep--;
-        tabactivator();
-
+        tabactivator(); //set appropriate tab to active
     });
+    //finishbutton eventlistener script
     $('#finishbutton').click(function() {
-        console.log('finishbuttonclick');
         location.reload();
     });
+
     function tabactivator(){
             if (currentstep == 0 ) {
-                $( "#tabs" ).tabs( "option", "disabled", [ 1, 2, 3 ] )
-
-                $( "#tabs" ).tabs({
-                    active: 0,
-                });
+                setactivetab(currentstep);
                 $('#previousbutton').prop("disabled", true)
-
             }
-
             if ((currentstep !== 0) && (currentstep !== maxstep )) {
-                $( "#tabs" ).tabs( "option", "disabled", [ 0, 2 ] )
-
-                $( "#tabs" ).tabs({
-                    active: 1,
-                });
+                setactivetab(currentstep);
                 $('#previousbutton').prop("disabled", false)
                 $('#finishbutton').prop("disabled", true)
                 $('#nextbutton').prop("disabled", false)
-
             }
-
             if (currentstep == maxstep ) {
-                $( "#tabs" ).tabs( "option", "disabled", [ 0, 1 ] )
-
-                $( "#tabs" ).tabs({
-                    active: 2,
-                });
+                setactivetab(currentstep);
                 $('#nextbutton').prop("disabled", true)
                 $('#finishbutton').prop("disabled", false)
-
             }
-
     }
-
+    function getactivetabtext(){
+        return $("#tabs .ui-tabs-active").text();
+    }
+    function setactivetabtextfilled(nextorprevious){
+            if (nextorprevious === 'next') {
+                $('#tabs ul:first li:eq(' + (currentstep) + ') a').text(getactivetabtext() + ' - OK'); //add ' - OK' to the end of tab
+            }
+            if (nextorprevious === 'previous') {
+                //set active the currentstep-1 tab to replace OK
+                setactivetab(currentstep-1);
+                //previous tab is active thus the 'OK' can be removed from active tab
+                $('#tabs ul:first li:eq(' + (currentstep-1) + ') a').text(getactivetabtext().replace(' - OK',''));
+            }
+    }
+    function setactivetab(xstep){
+        $( "#tabs" ).tabs({ disabled: false }); //set all tabs to enabled
+        $( "#tabs" ).tabs({
+            active: '' + (xstep) + '', //set tab active
+        });
+        $( "#tabs" ).tabs({ disabled: true }); //set all tab to disabled
+        $( "#tabs" ).tabs("enable", '' + (xstep + 1) + ''); //except this (this parameter is not 0 indexed so +1)
+    }
 });
